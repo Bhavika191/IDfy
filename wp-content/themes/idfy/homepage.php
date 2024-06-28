b5f44148c4300b92aaf12da48d0565662b7c3b4f
GiteshC <42336770+GiteshC@users.noreply.github.com>
2024-06-28 13:45:42 +0530

    platfor section dynamic

--- wp-content/themes/idfy/homepage.php
+++ wp-content/themes/idfy/homepage.php
@@ -91,164 +91,127 @@
 </section>
 
 <section class="onboardingJourneysec">
-    <div class="innerWrapper">
-        <div class="secHeading">
-            <h2>IDfy as a Platform</h2>
-            <p>Fighting Fraud at Scale with the Least Possible Friction</p>
-            <a href="#" class="ctaRed">Explore IDfy 360</a>
-        </div>
-        <div class="journeyData">
-            <div class="contentDiv">
-                <div class="innerBoxes">
-                    <h3>Plug & Play:</h3>
-                    <div class="journeyinnerData">
-                        <h4>Bring your own Stack</h4>
-                        <ul>
-                            <li>Integrate with your own systems easily</li>
-                            <li>Go-live in a jiffy</li>
-                        </ul>
+    <?php if( have_rows('idfy_platform_section') ): ?>
+        <?php while( have_rows('idfy_platform_section') ): the_row(); ?>
+            <div class="innerWrapper">
+                <div class="secHeading">
+                    <h2><?php echo get_sub_field('section_heading'); ?></h2>
+                    <p><?php echo get_sub_field('section_description'); ?></p>
+                    <a href="<?php echo get_sub_field('cta_link'); ?>" class="ctaRed"><?php echo get_sub_field('cta_text'); ?></a>
+                </div>
+                <div class="journeyData">
+                    <div class="contentDiv">
+                        <?php if( have_rows('left_pointers') ): ?>
+                            <?php while( have_rows('left_pointers') ): the_row(); ?>
+                                <div class="innerBoxes">
+                                    <h3><?php echo get_sub_field('card_heading'); ?></h3>
+                                    <div class="journeyinnerData">
+                                        <h4><?php echo get_sub_field('card_pointers_heading'); ?></h4>
+                                        <ul>
+                                            <?php if( have_rows('card_pointers') ): ?>
+                                                <?php while( have_rows('card_pointers') ): the_row(); ?>
+                                                    <li><?php echo get_sub_field('pointers_text'); ?></li>
+                                                <?php endwhile; ?>
+                                            <?php endif; ?>
+                                        </ul>
+                                    </div>
+                                </div>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
+                    </div>
+                    <div class="centerCirclediv">
+                        <div class="outerborderDiv">
+                            <div class="mainImg">
+                                <img src="<?php bloginfo('template_directory'); ?>/images/timeline-img.webp" alt="">
+                            </div>
+                            <?php if( have_rows('pointers_icon') ): ?>
+                                <?php while( have_rows('pointers_icon') ): the_row(); ?>
+                                    <div class="journeyIcons">
+                                        <?php $iconfield = get_sub_field('icon_field'); if( !empty( $iconfield ) ): ?>
+                                            <img src="<?php echo esc_url($iconfield['url']); ?>" loading="lazy" alt="<?php echo esc_attr($iconfield['alt']); ?>" />
+                                        <?php endif; ?>
+                                    </div>
+                                <?php endwhile; ?>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                    <div class="contentDiv">
+                        <?php if( have_rows('right_pointers') ): ?>
+                            <?php while( have_rows('right_pointers') ): the_row(); ?>
+                                <div class="innerBoxes">
+                                    <h3><?php echo get_sub_field('card_heading'); ?></h3>
+                                    <div class="journeyinnerData">
+                                        <h4><?php echo get_sub_field('card_pointers_heading'); ?></h4>
+                                        <ul>
+                                            <?php if( have_rows('card_pointers') ): ?>
+                                                <?php while( have_rows('card_pointers') ): the_row(); ?>
+                                                    <li><?php echo get_sub_field('pointers_text'); ?></li>
+                                                <?php endwhile; ?>
+                                            <?php endif; ?>
+                                        </ul>
+                                    </div>
+                                </div>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
                     </div>
                 </div>
-                <div class="innerBoxes">
-                    <h3>Comprehensive:</h3>
-                    <div class="journeyinnerData">
-                        <h4>End to End Solutions</h4>
-                        <ul>
-                            <li>Choose from our suite of 140+ onboarding and risk APIs</li>
-                            <li>Or bring your own APIs</li>
-                        </ul>
+                <div class="onboardingSliderData">
+                    <div class="centerCirclediv">
+                        <div class="outerborderDiv">
+                            <div class="mainImg">
+                                <img src="<?php bloginfo('template_directory'); ?>/images/timeline-img.webp" alt="">
+                            </div>
+                            <?php if( have_rows('pointers_icon') ): ?>
+                                <?php while( have_rows('pointers_icon') ): the_row(); ?>
+                                    <div class="journeyIcons">
+                                        <?php $iconfield = get_sub_field('icon_field'); if( !empty( $iconfield ) ): ?>
+                                            <img src="<?php echo esc_url($iconfield['url']); ?>" loading="lazy" alt="<?php echo esc_attr($iconfield['alt']); ?>" />
+                                        <?php endif; ?>
+                                    </div>
+                                <?php endwhile; ?>
+                            <?php endif; ?>
+                        </div>
                     </div>
-                </div>
-                <div class="innerBoxes">
-                    <h3>Customize:</h3>
-                    <div class="journeyinnerData">
-                        <h4>Build your journey from scratch</h4>
-                        <ul>
-                            <li>No-code; Drag & drop workflows></li>
-                            <li>White label the user experience</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="centerCirclediv">
-                <div class="outerborderDiv">
-                    <div class="mainImg">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/timeline-img.webp" alt="">
-                    </div>
-                    <div class="journeyIcons">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/journey-icon.svg" alt="">
-                    </div>
-                    <div class="journeyIcons">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/journey-icon.svg" alt="">
-                    </div>
-                    <div class="journeyIcons">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/journey-icon.svg" alt="">
-                    </div>
-                    <div class="journeyIcons">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/journey-icon.svg" alt="">
-                    </div>
-                    <div class="journeyIcons">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/journey-icon.svg" alt="">
-                    </div>
-                    <div class="journeyIcons">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/journey-icon.svg" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="contentDiv">
-                <div class="innerBoxes">
-                    <h3>Insights:</h3>
-                    <div class="journeyinnerData">
-                        <h4>Outsmart your competition</h4>
-                        <ul>
-                            <li>Gather Intel with real-time data dashboards</li>
-                            <li>Track and optimize your journeys</li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="innerBoxes">
-                    <h3>Mitigate Risk:</h3>
-                    <div class="journeyinnerData">
-                        <h4>The latest fraud-busting tech</h4>
-                        <ul>
-                            <li>Detect document tampering, impersonation and more</li>
-                            <li>Proprietary and predictive risk-modeling</li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="innerBoxes">
-                    <h3>Enterprise Ready:</h3>
-                    <div class="journeyinnerData">
-                        <h4>Grow with confidence</h4>
-                        <ul>
-                            <li>ISO and SOC 2, Type 2 certified platforms</li>
-                            <li> Highly resilient and highly scalable across process and tech</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="onboardingSliderData">
-            <div class="centerCirclediv">
-                <div class="outerborderDiv">
-                    <div class="mainImg">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/timeline-img.webp" alt="">
-                    </div>
-                    <div class="journeyIcons">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/journey-icon.svg" alt="">
-                    </div>
-                    <div class="journeyIcons">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/journey-icon.svg" alt="">
-                    </div>
-                    <div class="journeyIcons">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/journey-icon.svg" alt="">
-                    </div>
-                    <div class="journeyIcons">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/journey-icon.svg" alt="">
-                    </div>
-                    <div class="journeyIcons">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/journey-icon.svg" alt="">
-                    </div>
-                    <div class="journeyIcons">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/journey-icon.svg" alt="">
+                    <div class="onboardingSlider">
+                        <?php if( have_rows('left_pointers') ): ?>
+                            <?php while( have_rows('left_pointers') ): the_row(); ?>
+                                <div class="innerBoxes">
+                                    <h3><?php echo get_sub_field('card_heading'); ?></h3>
+                                    <div class="journeyinnerData">
+                                        <h4><?php echo get_sub_field('card_pointers_heading'); ?></h4>
+                                        <ul>
+                                            <?php if( have_rows('card_pointers') ): ?>
+                                                <?php while( have_rows('card_pointers') ): the_row(); ?>
+                                                    <li><?php echo get_sub_field('pointers_text'); ?></li>
+                                                <?php endwhile; ?>
+                                            <?php endif; ?>
+                                        </ul>
+                                    </div>
+                                </div>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
+                        <?php if( have_rows('right_pointers') ): ?>
+                            <?php while( have_rows('right_pointers') ): the_row(); ?>
+                                <div class="innerBoxes">
+                                    <h3><?php echo get_sub_field('card_heading'); ?></h3>
+                                    <div class="journeyinnerData">
+                                        <h4><?php echo get_sub_field('card_pointers_heading'); ?></h4>
+                                        <ul>
+                                            <?php if( have_rows('card_pointers') ): ?>
+                                                <?php while( have_rows('card_pointers') ): the_row(); ?>
+                                                    <li><?php echo get_sub_field('pointers_text'); ?></li>
+                                                <?php endwhile; ?>
+                                            <?php endif; ?>
+                                        </ul>
+                                    </div>
+                                </div>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
-            <div class="onboardingSlider">
-                <div class="innerBoxes">
-                    <h3>Insights:</h3>
-                    <div class="journeyinnerData">
-                        <h4>Outsmart your competition</h4>
-                        <ul>
-                            <li>Gather Intel with real-time data dashboards</li>
-                            <li>Track and optimize your journeys</li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="innerBoxes">
-                    <h3>Mitigate Risk:</h3>
-                    <div class="journeyinnerData">
-                        <h4>The latest fraud-busting tech</h4>
-                        <ul>
-                            <li>Detect document tampering, impersonation and more</li>
-                            <li>Proprietary and predictive risk-modeling</li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="innerBoxes">
-                    <h3>Enterprise Ready:</h3>
-                    <div class="journeyinnerData">
-                        <h4>Grow with confidence</h4>
-                        <ul>
-                            <li>ISO and SOC 2, Type 2 certified platforms</li>
-                            <li> Highly resilient and highly scalable across process and tech</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
 </section>
 
 <section class="productfamSection">
