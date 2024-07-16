5de13ce6de6ab2c86aff7c8898c550de7a66f601
Bhavika191 <bhavika.parmar@theminimalist.in>
2024-07-16 16:10:46 +0530

    css

--- wp-content/themes/idfy/footer.php
+++ wp-content/themes/idfy/footer.php
@@ -143,13 +143,55 @@
 
 </div><!-- #page -->
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery-3.7.1.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollTrigger.min.js"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollTrigger.min.js"></script> -->
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/slick.min.js"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/custom.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/studio-freight/lenis@0.2.28/bundled/lenis.js"></script>
+
+<script>
+	const lenis = new Lenis({
+		duration: 1.2,
+		easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+		direction: 'vertical', // vertical, horizontal
+		gestureDirection: 'vertical', // vertical, horizontal, both
+		smooth: true,
+		mouseMultiplier: 1,
+		smoothTouch: false,
+		touchMultiplier: 2,
+		infinite: false,
+	})
+
+	//get scroll value
+	lenis.on('scroll', ({
+		scroll,
+		limit,
+		velocity,
+		direction,
+		progress
+	}) => {
+		console.log({
+			scroll,
+			limit,
+			velocity,
+			direction,
+			progress
+		})
+	})
+
+	function raf(time) {
+		lenis.raf(time)
+		requestAnimationFrame(raf)
+	}
+
+	requestAnimationFrame(raf)
+</script>
 
 <?php wp_footer(); ?>
 
 </body>
+
+
+
 
 </html>
